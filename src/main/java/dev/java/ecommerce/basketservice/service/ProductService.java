124b35245237d4d84201a1308321a0490db7163d
juliolouzz <jlouz.dev@gmail.com>
2025-10-24 21:07:50 +0100

    feat: Creates 'ProductService' to add method to search all products and method to search for product by id

--- src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
+++ src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
@@ -1,4 +1,15 @@
 package dev.java.ecommerce.basketservice.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+    public void getAllProducts() {
+        // Implementation will go here
+    }
+
+    public void getProductById(Long id) {
+        // Implementation will go here
+    }
 }
