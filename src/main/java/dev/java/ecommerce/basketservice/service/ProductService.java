30b11099d754254a8df968e6464493f64a7afcd9
juliolouzz <jlouz.dev@gmail.com>
2025-10-25 10:59:06 +0100

    feat: Implements the integration with the Fake Store API to fetch products.
    - Create PlatziStoreClient
    
    - Add API URL configuration
    
    - Create methods to fetch all products and product by ID
    
    - Adjust ProductService and ProductController to consume the dataCreates 'ProductService' to add method to search all products and method to search for product by id

--- src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
+++ src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
@@ -1,15 +1,23 @@
 package dev.java.ecommerce.basketservice.service;
 
+import dev.java.ecommerce.basketservice.client.PlatziStoreClient;
+import dev.java.ecommerce.basketservice.client.response.PlatziProductResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class ProductService {
 
-    public void getAllProducts() {
-        // Implementation will go here
+    private final PlatziStoreClient platziStoreClient;
+
+    public List<PlatziProductResponse> getAllProducts() {
+        return platziStoreClient.getAllProducts();
     }
 
-    public void getProductById(Long id) {
-        // Implementation will go here
+    public PlatziProductResponse getProductById(Long id) {
+        return platziStoreClient.getProductById(id);
     }
 }
