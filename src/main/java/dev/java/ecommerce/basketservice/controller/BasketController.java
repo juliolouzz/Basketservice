c26950304654cd84bfaa2fb7cb2170a72a1a0953
juliolouzz <jlouz.dev@gmail.com>
2025-10-26 21:32:44 +0000

    feat: Creates 'BasketController' and 'BasketService' to add endpoint and methods to add search basket by id and to create basket

---
+++ src/main/java/dev/java/ecommerce/basketservice/controller/BasketController.java
@@ -0,0 +1,27 @@
+package dev.java.ecommerce.basketservice.controller;
+
+import dev.java.ecommerce.basketservice.controller.request.BasketRequest;
+import dev.java.ecommerce.basketservice.entity.Basket;
+import dev.java.ecommerce.basketservice.service.BasketService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/basket")
+@RequiredArgsConstructor
+public class BasketController {
+
+    private final BasketService basketService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Basket> getBasketById(@PathVariable String id) {
+        return ResponseEntity.ok(basketService.getBasketById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Basket> getAllBaskets(@RequestBody BasketRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(basketService.createBasket(request));
+    }
+}
